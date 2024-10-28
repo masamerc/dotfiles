ca82365dc278ea06176801a6bf0b697dd3710413
Masa Fukui <masahiro.fukui@nowcast.co.jp>
2024-10-28 17:29:44 +0900

    config colors override

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -16,8 +16,8 @@ config.macos_window_background_blur = 40
 
 -- appearance
 -- config.color_scheme = 'Catppuccin  (Gogh)'
-config.color_scheme = "nightfox"
-config.color_scheme = 'rose-pine'
+-- config.color_scheme = "nightfox"
+
 config.font = wezterm.font_with_fallback({
     {family="JetBrainsMono Nerd Font", weight="DemiBold"},
     {family="Twemoji Mozilla", assume_emoji_presentation = true},
@@ -27,6 +27,20 @@ config.window_decorations = 'RESIZE'
 config.colors = {
   background = '#1f1f1f',
 }
+
+
+-- custom scheme
+local scheme = wezterm.color.get_builtin_schemes()['rose-pine']
+scheme.ansi = {
+    "#26233a","#eb6f92","#9ccfd8","#f6c177","#3eb1b4","#c4a7e7","#ebbcba","#e0def4"
+}
+scheme.brights = {
+    "#6e6a86","#eb6f92","#9ccfd8","#f6c177","#3eb1b4","#c4a7e7","#ebbcba","#e0def4"
+}
+config.color_schemes = {
+    ['rose-pine'] = scheme,
+}
+config.color_scheme = 'rose-pine'
 
 -- keys
 local act = wezterm.action
@@ -39,3 +53,5 @@ config.keys = {
 }
 
 return config
+
+
