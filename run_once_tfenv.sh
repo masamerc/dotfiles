1c98f156abf3d477c95fe756c43b73a304119ba4
Masa Fukui <masahiro.m.fukui@gmail.com>
2024-12-25 21:21:56 +0900

    update path

--- run_once_tfenv.sh
+++ run_once_tfenv.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
-echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.zprofile
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.zshrc
